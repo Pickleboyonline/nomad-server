a69f583617ed031fe44ad26a2b4b5b5032cd28d4
Pickleboyonline <pickleboyonline@gmail.com>
2017-03-18 08:05:11 -0500

    can now serve files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,15 @@ public class Main {
     public static void main(String[] args) {
         char mark = '\u0022';
         Database db = new Database();
-        get("/apps", (request, response) -> {
+        staticFiles.externalLocation("C:\\apps");
+        get("/data", (request, response) -> {
 
             return db.getMetadata(request.queryParams("id"));
         });
+
+
+        System.out.println("C:\\apps");
+
         get("/link", (request, response) -> {
 
             return "<a href=" + mark + "nomadapp://open?key=value&id=58cc05675e6d84d8c21f8bb5"+ mark + ">Link</a>";
